ec1b4be0693eb1e5063bbd76ef4a4e1f32914b34
theMashrur <shafi0802@gmail.com>
2022-04-18 14:29:58 +0100

    finished string and multiplication implementation

--- week3/polynomials.dart
+++ week3/polynomials.dart
@@ -2,8 +2,7 @@ import 'dart:math';
 import 'package:collection/collection.dart';
 
 main() {
-  Polynomial([1]) + Polynomial([1]);
-  Polynomial([1]) + 1;
+  print(Polynomial([1, 2, 3, 4]));
 }
 
 class Polynomial {
@@ -15,6 +14,22 @@ class Polynomial {
 
   int degree() {
     return this.coefficients.length - 1;
+  }
+
+  String toString() {
+    var str = '';
+    var rcoefs = List.from(coefficients.reversed);
+    rcoefs.forEachIndexed((index, element) {
+      if (index == (rcoefs.length - 1)) {
+        var current = rcoefs[index];
+        str = str + "$current";
+      } else {
+        var current = rcoefs[index];
+        var power = rcoefs.length - index - 1;
+        str = str + "$current*x^$power + ";
+      }
+    });
+    return str;
   }
 
   operator +(other) {
@@ -32,6 +47,20 @@ class Polynomial {
     } else if (other is double) {
       return Polynomial(
           [(this.coefficients[0] + other)] + this.coefficients.sublist(1));
+    }
+  }
+
+  operator *(other) {
+    if (other is Polynomial) {
+      var t = this.coefficients;
+      var o = other.coefficients;
+      var prod = List.filled(t.length + o.length, 0);
+      t.forEachIndexed((spower, sco) {
+        o.forEachIndexed((opower, oco) {
+          prod[spower + opower] = (sco * oco) + prod[spower + opower];
+        });
+      });
+      return Polynomial(prod);
     }
   }
 }
