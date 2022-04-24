2b87da6b9d97577d9dcd515f3a0b74bb8f7dc43f
theMashrur <shafi0802@gmail.com>
2022-04-24 18:27:22 +0100

    moved circle to separate file

--- week3/polynomials.dart
+++ week3/polynomials.dart
@@ -107,21 +107,3 @@ class Polynomial {
     return Polynomial(deriv);
   }
 }
-
-class Circle {
-  var centre = [];
-  num radius = 0;
-
-  Circle(this.centre, this.radius);
-
-  bool contains(point) {
-    var x1 = point[0];
-    var y1 = point[1];
-    var x0 = centre[0];
-    var y0 = centre[1];
-    if (pow(x1 - x0, 1) + pow(y1 - y0, 2) < pow(radius, 2)) {
-      return true;
-    }
-    return false;
-  }
-}
